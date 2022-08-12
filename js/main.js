47f192e55d4eee06dbfeee14f6f6bcb7d80b43a3
Ania <an.pasieczna@gmail.com>
2022-08-12 20:52:14 +0200

    Obtaining the pizza value from the select tag

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+//Obtain the selected pizza option from the HTML select tag:
+const btn = document.querySelector('.submit-btn');
+console.log(btn)
+function getPizza() {
+const pizzaSelect = document.getElementById('pizza-select')
+const pizzaValue = pizzaSelect.value;
+console.log(pizzaValue)
+return pizzaValue
+}
+btn.addEventListener('click', getPizza)
+
+//const selectPizza = document.getElementById('pizza-select');
+//selectPizza.addEventListener('change', function handleChange (event){
+    //console.log(event.target.value);
+    //console.log(selectPizza.options[selectPizza.selectedIndex].text);
+//})
+
+//const chooseSauce = document.querySelectorAll()
 class Order {
     constructor(pizza, sauce, name, email, address, phone, drink) {
         this.pizza= pizza
